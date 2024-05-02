30959b5513575299d1011c51300afdd06006ee8d
Brayan Diaz C <brayandiazc@gmail.com>
2024-05-02 17:46:28 -0500

    [FEAT] App de asignacion_vehiculos creada

--- domicilioApp/domicilioApp/settings.py
+++ domicilioApp/domicilioApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "asignacion_vehiculos.apps.AsignacionVehiculosConfig",
 ]
 
 MIDDLEWARE = [
